e4afe5042f49ee3de904b6e296828807bec0624f
Merin-jai <145257949+Merin-jai@users.noreply.github.com>
2025-02-18 14:52:17 +0530

    metadata

--- src/App.js
+++ src/App.js
@@ -28,3 +28,13 @@ export default App;
 // The useSelector hook takes a function as an argument, which is used to select the state value fromthe Redux store. The function takes the state as an argument and returns the value you want to retrieve.
 // The useDispatch hook returns a dispatch function, which is used to dispatch actions to the Redux store. The dispatch function takes an action as an argument and dispatches it to the Redux store. The action is an object that contains the data you want to modify in the Redux stat
 // Redux Actions (increment, decrement, incrementByAmount) → Imported from counterSlice.js, these update the state.
+
+
+// useSelector() allows the component to access Redux state.
+// state.counter.value → Retrieves the value from the counter slice in the Redux store.
+
+
+// const dispatch = useDispatch();
+// useDispatch() gives access to Redux’s dispatch() function.
+// It is used to trigger actions (increment, decrement, etc.).
+// dispatch(increment()) → Dispatches the increment action to the Redux store.
--- src/index.js
+++ src/index.js
@@ -19,3 +19,15 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+// Provider is like a supervisor for your Redux store. It's a special component provided by the React Redux library that wraps around your entire React application. This Provider component ensures that all components inside your app can access the Redux store and its state without having to pass it down manually through component props.
+
+// Providers in React Redux:
+// The Provider in React Redux is a special component that wraps around your entire React application.
+// It ensures that all components within your app have access to the Redux store without needing to pass it down manually through props.
+// The Provider acts as a gateway or supervisor, granting access to the Redux store to any component that needs it.
+// It simplifies the process of managing state in large React applications by centralizing access to the Redux store.
+// By using the Provider, components can easily access and modify the state stored in Redux, making it easier to build and maintain complex applications.
